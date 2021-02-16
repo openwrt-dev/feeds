15991706942fba9cf12fb8a6e3442bffca5f6531
pexcn <i@pexcn.me>
2021-02-17 01:33:40 +0800

    Migrate to GitHub Actions (#6)

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -e
 set -o pipefail
 
-TARGET=$(cut -d '/' -f 7-8 <<< $SDK_URL | tr '/' '-')
-SDK_DIR=openwrt-sdk-$TARGET
+SDK_DIR=/tmp/openwrt-sdk
 
 get_sdk() {
   curl -sSL $SDK_URL | tar Jxf -
-  mv openwrt-sdk* $SDK_DIR
+  mv openwrt-sdk-* $SDK_DIR
 }
 
-get_sources() {
-  cd $SDK_DIR
-  git clone https://github.com/openwrt-dev/feeds.git package/openwrt-dev -b master --single-branch --recurse-submodules -j4
-  cp ../key-build .
-  cd ..
+copy_sources() {
+  mkdir -p $SDK_DIR/package/openwrt-dev
+  cp -r libs net $SDK_DIR/package/openwrt-dev
+  cp key-build $SDK_DIR
 }
 
 build_packages() {
   cd $SDK_DIR
 
-  make defconfig
+  make defconfig V=w
 
   make package/libev/compile V=w
   make package/libcares/compile V=w
@@ -41,29 +39,12 @@ build_packages() {
   make package/openwrt-dns2socks/compile V=w
   make package/openwrt-portfwd/compile V=w
   make package/openwrt-vlmcsd/compile V=w
-
-  make package/openwrt-shadowsocks/compile V=w \
-    CONFIG_SHADOWSOCKS_STATIC_LINK=y \
-    CONFIG_SHADOWSOCKS_WITH_EV=y \
-    CONFIG_SHADOWSOCKS_WITH_PCRE=y \
-    CONFIG_SHADOWSOCKS_WITH_CARES=y \
-    CONFIG_SHADOWSOCKS_WITH_SODIUM=y \
-    CONFIG_SHADOWSOCKS_WITH_MBEDTLS=y
-
-  make package/openwrt-simple-obfs/compile V=w \
-    CONFIG_SIMPLE_OBFS_STATIC_LINK=y
+  make package/openwrt-shadowsocks/compile V=w
+  make package/openwrt-simple-obfs/compile V=w
 
   make package/index V=s
-
-  cd ..
-}
-
-copy_binaries() {
-  mkdir $TARGET
-  cp -r $SDK_DIR/bin/packages/*/base $TARGET
 }
 
 get_sdk
-get_sources
+copy_sources
 build_packages
-copy_binaries
