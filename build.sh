1ea52f3f8bf4e220a41b78d61f19d3da8d622c78
pexcn <i@pexcn.me>
2021-02-17 16:27:32 +0800

    make with multi-threads

--- build.sh
+++ build.sh
@@ -19,28 +19,28 @@ build_packages() {
 
   make defconfig V=w
 
-  make package/libev/compile V=w
-  make package/libcares/compile V=w
-  make package/libsodium/compile V=w
-  make package/mbedtls/compile V=w
-  make package/pcre/compile V=w
+  make -j$(nproc) package/libev/compile V=w
+  make -j$(nproc) package/libcares/compile V=w
+  make -j$(nproc) package/libsodium/compile V=w
+  make -j$(nproc) package/mbedtls/compile V=w
+  make -j$(nproc) package/pcre/compile V=w
 
-  make package/luci-app-shadowsocks/compile V=w
-  make package/openwrt-dist-luci/compile V=w
+  make -j$(nproc) package/luci-app-shadowsocks/compile V=w
+  make -j$(nproc) package/openwrt-dist-luci/compile V=w
 
-  make package/openwrt-chinadns/compile V=w
-  make package/openwrt-chinadns-ng/compile V=w
-  make package/openwrt-dns-forwarder/compile V=w
-  make package/openwrt-dns2tcp/compile V=w
-  make package/openwrt-ipt2socks/compile V=w
-  make package/openwrt-hev-socks5-server/compile V=w
-  make package/openwrt-udp2raw/compile V=w
-  make package/openwrt-udpspeeder/compile V=w
-  make package/openwrt-dns2socks/compile V=w
-  make package/openwrt-portfwd/compile V=w
-  make package/openwrt-vlmcsd/compile V=w
-  make package/openwrt-shadowsocks/compile V=w
-  make package/openwrt-simple-obfs/compile V=w
+  make -j$(nproc) package/openwrt-chinadns/compile V=w
+  make -j$(nproc) package/openwrt-chinadns-ng/compile V=w
+  make -j$(nproc) package/openwrt-dns-forwarder/compile V=w
+  make -j$(nproc) package/openwrt-dns2tcp/compile V=w
+  make -j$(nproc) package/openwrt-ipt2socks/compile V=w
+  make -j$(nproc) package/openwrt-hev-socks5-server/compile V=w
+  make -j$(nproc) package/openwrt-udp2raw/compile V=w
+  make -j$(nproc) package/openwrt-udpspeeder/compile V=w
+  make -j$(nproc) package/openwrt-dns2socks/compile V=w
+  make -j$(nproc) package/openwrt-portfwd/compile V=w
+  make -j$(nproc) package/openwrt-vlmcsd/compile V=w
+  make -j$(nproc) package/openwrt-shadowsocks/compile V=w
+  make -j$(nproc) package/openwrt-simple-obfs/compile V=w
 
   make package/index V=s
 
