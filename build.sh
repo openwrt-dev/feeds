d0c939d89650e1b9c4f2827ebaa6962f6bcc7452
pexcn <i@pexcn.me>
2021-02-17 20:17:16 +0800

    update build order

--- build.sh
+++ build.sh
@@ -31,16 +31,16 @@ build_packages() {
   make -j$(nproc) package/openwrt-chinadns/compile V=w
   make -j$(nproc) package/openwrt-chinadns-ng/compile V=w
   make -j$(nproc) package/openwrt-dns-forwarder/compile V=w
-  make -j$(nproc) package/openwrt-dns2tcp/compile V=w
-  make -j$(nproc) package/openwrt-ipt2socks/compile V=w
-  make -j$(nproc) package/openwrt-hev-socks5-server/compile V=w
-  make -j$(nproc) package/openwrt-udp2raw/compile V=w
-  make -j$(nproc) package/openwrt-udpspeeder/compile V=w
   make -j$(nproc) package/openwrt-dns2socks/compile V=w
+  make -j$(nproc) package/openwrt-dns2tcp/compile V=w
+  make -j$(nproc) package/openwrt-hev-socks5-server/compile V=w
+  make -j$(nproc) package/openwrt-ipt2socks/compile V=w
   make -j$(nproc) package/openwrt-portfwd/compile V=w
-  make -j$(nproc) package/openwrt-vlmcsd/compile V=w
   make -j$(nproc) package/openwrt-shadowsocks/compile V=w
   make -j$(nproc) package/openwrt-simple-obfs/compile V=w
+  make -j$(nproc) package/openwrt-udp2raw/compile V=w
+  make -j$(nproc) package/openwrt-udpspeeder/compile V=w
+  make -j$(nproc) package/openwrt-vlmcsd/compile V=w
 
   make package/index V=s
 
